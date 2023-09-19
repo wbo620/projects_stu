331c3df45c4b9d1e66ffde62521f0cf1b2f76273
wbo620 <468308027@qq.com>
2023-09-19 21:11:55 +0800

    初始化后端

---
+++ iceso/iceso-backend/src/main/java/com/ice/springbootinit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.ice.springbootinit.exception;
+
+import com.ice.springbootinit.common.BaseResponse;
+import com.ice.springbootinit.common.ErrorCode;
+import com.ice.springbootinit.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+  *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ iceso/iceso-backend/src/main/java/com/ice/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ice.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ice.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+  *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
