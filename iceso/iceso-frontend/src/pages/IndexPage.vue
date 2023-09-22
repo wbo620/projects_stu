96ec392b8ad1689fe3b6095a3b9411a467362fc5
wbo620 <468308027@qq.com>
2023-09-22 19:48:14 +0800

    前端请求接口统一，优化代码

--- iceso/iceso-frontend/src/pages/IndexPage.vue
+++ iceso/iceso-frontend/src/pages/IndexPage.vue
@@ -7,29 +7,73 @@ import PostList from "@/components/PostList.vue";
 import UserList from "@/components/UserList.vue";
 import { useRoute, useRouter } from "vue-router";
 import myAxios from "@/plugins/myAxios";
+import { message } from "ant-design-vue";
 
 const postlist = ref([]);
 const picturelist = ref([]);
 const userlist = ref([]);
-//展示文章信息
+/*//展示文章信息
 myAxios.post("post/list/page/vo", {}).then((re: any) => {
   postlist.value = re.records;
 });
 myAxios.post("user/list/page/vo", {}).then((re: any) => {
   userlist.value = re.records;
-});
+});*/
+/**
+ * 加载单类数据
+ * @param params
+ */
+const loadData = (params: any) => {
+  const { type = "psost" } = params;
+  if (!type) {
+    message.error("类别为空");
+    return;
+  }
+  const query = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("search/all", query).then((res: any) => {
+    if (type === "post") {
+      postlist.value = res.postList;
+    } else if (type === "user") {
+      userlist.value = res.userList;
+    } else if (type === "picture") {
+      picturelist.value = res.pictureList;
+    }
+  });
+};
+/**
+ * 加载聚合数据
+ * @param params
+ */
+const loadAllData = (params: any) => {
+  const query = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("search/all", query).then((res: any) => {
+    postlist.value = res.dataList;
+
+    userlist.value = res.dataList;
+
+    picturelist.value = res.dataList;
+  });
+};
 
 const router = useRouter();
 const route = useRoute();
 const searchText = ref(route.query.text || "");
-// 初始地址栏参数
-const initSearchParams = {
-  pageSize: 10,
-  pageNum: 1,
-  text: "",
-};
 // 动态获取路由参数
 const activeKey = route.params.category;
+// 初始地址栏参数
+const initSearchParams = {
+  type: activeKey,
+  text: "",
+  pageSize: 10,
+  pageNum: 1,
+};
+
 const searchParams = ref(initSearchParams);
 
 // 地址栏参数与页面对应
@@ -37,7 +81,9 @@ watchEffect(() => {
   searchParams.value = {
     ...initSearchParams,
     text: route.query.text,
+    type: route.params.category,
   } as any;
+  loadData(searchParams.value);
 });
 // 搜索栏事件
 const onSearch = (value: string) => {
