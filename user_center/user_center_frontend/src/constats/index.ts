26bf98730c5f64b4af3c752d0d7fce9ef14900a4
wbo620 <468308027@qq.com>
2023-10-11 12:13:00 +0800

    1.0

--- user_center/user_center_frontend/src/constats/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 登录页logo
- */
-export const SYSTEM_LOG="https://lug.ustc.edu.cn/static/lug-penguin.png";
-
-/**
- * logo下面的链接地址--gitee地址
- */
-export const PLANET_NET="https://gitee.com/wbo620";
