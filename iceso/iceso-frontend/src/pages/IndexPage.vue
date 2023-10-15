625f61b49aca68d91092863bc00f08d05cb41f48
wbo620 <468308027@qq.com>
2023-10-15 19:52:35 +0800

    前端修复地址栏type类型是之前页面的bug 初始页面设置为post

--- iceso/iceso-frontend/src/pages/IndexPage.vue
+++ iceso/iceso-frontend/src/pages/IndexPage.vue
@@ -1,57 +1,58 @@
 <template>
   <div class="index-page">
-    <!-- 搜索建议，选中之后自动搜索 -->
     <div>
       <a-auto-complete
         v-model:value="searchText"
         :options="options"
-        placeholder="请输入搜索关键词"
         @select="onSearch"
         size="large"
         style="width: 90%"
-      />
-      <!-- 点击搜索，对应不选建议，直接点击搜索事件 -->
-      <a-button
-        type="primary"
-        style="font-size: 16px; height: 40px; width: 10%"
-        @click="onSearch(searchText)"
-        >搜索
-      </a-button>
+      >
+        <a-input-search
+          v-model:value="searchParams"
+          placeholder="请输入搜索关键词"
+          enter-button="搜索"
+          size="large"
+          @search="onSearch"
+        />
+      </a-auto-complete>
+      <MyDivider />
+      <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
+        <a-tab-pane key="post" tab="文章">
+          <PostList :post-list="postlist" />
+        </a-tab-pane>
+        <a-tab-pane key="picture" tab="图片">
+          <PictureList :picture-list="picturelist" />
+        </a-tab-pane>
+        <a-tab-pane key="user" tab="用户">
+          <UserList :user-list="userlist" />
+        </a-tab-pane>
+        <a-tab-pane key="video" tab="视频">
+          <VideoList :video-list="videolist" />
+        </a-tab-pane>
+      </a-tabs>
     </div>
-    <MyDivider />
-    <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
-      <a-tab-pane key="post" tab="文章">
-        <PostList :post-list="postlist" />
-      </a-tab-pane>
-      <a-tab-pane key="picture" tab="图片">
-        <PictureList :picture-list="picturelist" />
-      </a-tab-pane>
-      <a-tab-pane key="user" tab="用户">
-        <UserList :user-list="userlist" />
-      </a-tab-pane>
-      <a-tab-pane key="video" tab="视频">
-        <VideoList :video-list="videolist" />
-      </a-tab-pane>
-    </a-tabs>
   </div>
 </template>
-<script setup lang="ts">
-import { ref, watch, watchEffect } from "vue";
 
-import PictureList from "@/components/PictureList.vue";
-import MyDivider from "@/components/MyDivider.vue";
-import PostList from "@/components/PostList.vue";
-import UserList from "@/components/UserList.vue";
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import myAxios from "@/plugins/myAxios";
 import { message } from "ant-design-vue";
+import UserList from "@/components/UserList.vue";
 import VideoList from "@/components/VideoList.vue";
+import PictureList from "@/components/PictureList.vue";
+import PostList from "@/components/PostList.vue";
+import MyDivider from "@/components/MyDivider.vue";
 
 const postlist = ref([]);
 const picturelist = ref([]);
 const userlist = ref([]);
 const videolist = ref([]);
 const options = ref<any>([]);
+// 初始化一个标志来跟踪是否是第一次加载
+let isFirstLoad = true;
 
 const router = useRouter();
 const route = useRoute();
@@ -62,54 +63,36 @@ const activeKey = route.params.category;
 const initSearchParams = {
   type: activeKey,
   text: "",
-  pageSize: 10,
+  pageSize: 20,
   pageNum: 1,
 };
+//初始化查找参数
+const searchParams = ref(initSearchParams);
+//查找文本
 const searchText = ref(route.query.text || "");
-/*//加载数据
-myAxios.post("post/list/page/vo", {}).then((re: any) => {
-  postlist.value = re.records;
-});
-myAxios.post("user/list/page/vo", {}).then((re: any) => {
-  userlist.value = re.records;
-});*/
-// 在tab为post时，监听搜索内容，开启搜索建议
-
-/**
- * 加载聚合数据
- * @param params
- */
-const loadAllData = (params: any) => {
-  // const { type = "post" } = params;
-  // if (!type) {
-  //   message.error("类别为空");
-  //   return;
-  // }
-  const query = {
-    ...params,
-    searchText: params.text,
-  };
-  myAxios.post("search/all", query).then((res: any) => {
-    postlist.value = res.postList;
-    userlist.value = res.userList;
-    picturelist.value = res.pictureList;
-    videolist.value = res.videoList;
-  });
-};
 /**
  * 加载单类数据
  * @param params
  */
+
 const loadData = (params: any) => {
   const { type = "post" } = params;
+
   if (!type) {
     message.error("类别为空");
     return;
+  }
+
+  if (isFirstLoad) {
+    // 第一次加载时默认加载"post"
+    searchParams.value.type = "post";
+    isFirstLoad = false;
   }
   const query = {
     ...params,
     searchText: params.text,
   };
+
   myAxios.post("search/all", query).then((res: any) => {
     if (type === "post") {
       postlist.value = res.dataList;
@@ -123,11 +106,7 @@ const loadData = (params: any) => {
   });
 };
 
-//初始化查找参数
-const searchParams = ref(initSearchParams);
-// //首次请求加载页面
-//loadAllData(initSearchParams);
-// 地址栏参数与页面对应
+// 监听地址栏：地址栏参数与页面对应
 //作用：当地址改变，重新加载页面
 watchEffect(() => {
   searchParams.value = {
@@ -135,64 +114,32 @@ watchEffect(() => {
     text: route.query.text,
     type: route.params.category,
   } as any;
-  //点击搜索栏再加载页面
+  //路径改变，加载页面
   loadData(searchParams.value);
 });
 
-// // 搜索建议
-// watch(
-//   () => searchParams.value.text,
-//   (newValue) => {
-//     if (activeKey === "post") {
-//       // 启用监听
-//       myAxios
-//         .get("/suggest/post?pre=" + searchParams.value.text)
-//         .then((res: any) => {
-//           //console.log(res);
-//           // 清空之前的options
-//           options.value = [];
-//           res.forEach((item: string) => {
-//             options.value.push({ value: item });
-//           });
-//         });
-//       console.log("监听已启动");
-//     } else {
-//       // 停止监听
-//       console.log("监听已停止");
-//     }
-//   }
-// );
-// 搜索栏事件：点击搜索，触发事件
-const onSearch = (value: any) => {
+// 搜索栏事件：点击搜索，路径改变
+const onSearch = (value: string) => {
   router.push({
     query: {
       ...searchParams.value,
       text: value,
     },
   });
-  //loadData(searchParams.value);
 };
+//逻辑
+//点击搜索，搜索参数text发生改变，路径改变，被监听，执行后端请求
+//Tab改变，activeKey改变，路径改变，被监听，执行搜索
 
-// const getSearchPrompt = (value: string) => {
-//   options.value = [];
-//   console.log(value);
-//   if (value) {
-//     myAxios.get("search/getSearchPrompt?keyword=" + value).then((res: any) => {
-//       for (let i = 0; i < res.length; i++) {
-//         const tempMap = {
-//           value: res[i],
-//           color: "red",
-//         };
-//         options.value.push(tempMap);
-//       }
-//     });
-//   }
-// };
 //列表页切换事件：地址参数改变
 const onTabChange = (key: string) => {
+  //searchParams.value.type = `${key}`;
   router.push({
     path: `/${key}`,
-    query: searchParams.value,
+    query: {
+      ...searchParams.value,
+      type: `${key}`,
+    },
   });
 };
 </script>
